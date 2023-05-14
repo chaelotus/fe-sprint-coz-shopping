d2ea0a1ecfc7a6423b0721a0581a48ab63a1ec63
parkchaeyeon <pcy6904@naver.com>
2023-05-14 18:22:07 +0900

    드롭다운 구현

--- shopping-app/src/page/Header.js
+++ shopping-app/src/page/Header.js
@@ -1,16 +1,52 @@
 import "./Header.css";
+import { useState, useRef, useEffect } from "react";
+import { MdStarOutline, MdOutlineCardGiftcard } from "react-icons/md";
 const Header = () => {
+  const [isClick, SetIsClick] = useState(false);
+  const outsideRef = useRef();
+
+  useEffect(() => {
+    const outsideClick = (e) => {
+      if (isClick && !outsideRef.current.contains(e.target)) {
+        SetIsClick(false);
+      }
+    };
+    document.addEventListener("mousedown", outsideClick);
+    return () => {
+      document.removeEventListener("mousedown", outsideClick);
+    };
+  }, [isClick]);
+
+  const clickMenuIcon = () => {
+    SetIsClick(true);
+  };
   return (
     <header className="header">
       <div className="header-column">
-        <img className="header-logoImg" src={require("../img/logo.png")} />
-        <div className="header-title">COZ Shopping</div>
+        <div className="header-mainTitle">
+          <img className="header-logoImg" src={require("../img/logo.png")} />
+          <div className="header-title">COZ Shopping</div>
+        </div>
       </div>
       <div className="header-column">
         <img
+          onClick={clickMenuIcon}
           className="header-menuIcon"
           src={require("../img/menu-icon.png")}
         />
+        {isClick && (
+          <section className="dropDown" ref={outsideRef}>
+            <div>ooo님, 안녕하세요!</div>
+            <div>
+              <MdOutlineCardGiftcard className="dropDown-Icon" />
+              <span>상품리스트 페이지</span>
+            </div>
+            <div>
+              <MdStarOutline className="dropDown-Icon" />
+              <span>북마크 페이지</span>
+            </div>
+          </section>
+        )}
       </div>
     </header>
   );
