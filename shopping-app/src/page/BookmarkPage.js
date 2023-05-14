bd1c3c77ce118949edc3c48b46ccf5e73f2e463a
parkchaeyeon <pcy6904@naver.com>
2023-05-15 01:27:00 +0900

    feat : footer 구현

--- shopping-app/src/page/BookmarkPage.js
+++ shopping-app/src/page/BookmarkPage.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./Header";
-
+import Footer from "./Footer";
 const BookmarkPage = () => {
   return (
-    <>
-      <Header />
-      <span>bookmark</span>
-    </>
+    <div className="wrapper">
+      <div className="contentWrapper">
+        <Header className="header" />
+        <div className="main">bookmark-page</div>
+      </div>
+
+      <Footer></Footer>
+    </div>
   );
 };
 export default BookmarkPage;
