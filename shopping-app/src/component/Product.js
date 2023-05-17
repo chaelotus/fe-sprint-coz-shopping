900cb7babfcbe2b17268fb618871f38af05938f0
parkchaeyeon <pcy6904@naver.com>
2023-05-17 17:33:56 +0900

    feat : product category 필터 구현

--- shopping-app/src/component/Product.js
+++ shopping-app/src/component/Product.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { MdStar } from "react-icons/md";
 import Modal from "./Modal";
 import "./Product.css";
--- shopping-app/src/page/ProductPage.js
+++ shopping-app/src/page/ProductPage.js
@@ -1,11 +1,25 @@
-import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
+import React, { useEffect, useState } from "react";
+import "./Productpage.css";
+import ProductCategory from "../component/ProductCategory";
+import Product from "../component/Product";
 const ProductPage = () => {
+  const bookmark = [];
+  const [productList, setProductList] = useState([]);
+  useEffect(() => {
+    fetch("http://cozshopping.codestates-seb.link/api/v1/products?count=10")
+      .then((res) => res.json())
+      .then((data) => setProductList(data));
+  }, []);
+
   return (
     <div className="wrapper">
-      <div className="contentWrapper">
-        <div className="main">product-page</div>
+      <div className="productContent">
+        <div className="category-container">
+          <ProductCategory />
+        </div>
+        <div className="category-product-list">
+          <Product productData={productList} getBookmarkItem={bookmark} />
+        </div>
       </div>
     </div>
   );
