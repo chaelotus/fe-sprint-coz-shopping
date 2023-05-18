352df6cbad42437d0a7ff6dbbfd4a962a3045811
parkchaeyeon <pcy6904@naver.com>
2023-05-18 23:02:10 +0900

    feat : bookmarkPage 페이지 구성

--- shopping-app/src/page/BookmarkPage.js
+++ shopping-app/src/page/BookmarkPage.js
@@ -1,11 +1,39 @@
-import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
+import React, { useState, useEffect } from "react";
+import ProductCategory from "../component/ProductCategory";
+import Product from "../component/Product";
+import "./BookmarkPage.css";
 const BookmarkPage = () => {
+  const [bookmarkList, setBookmarkList] = useState([]);
+  const [categoryType, setCategoryType] = useState("All");
+  const [filtered, setFiltered] = useState([]);
+
+  useEffect(() => {
+    setBookmarkList(JSON.parse(localStorage.getItem("bookmark")));
+  }, []);
+  console.log(bookmarkList);
+  useEffect(() => {
+    if (categoryType === "All") {
+      setFiltered(bookmarkList);
+    } else {
+      setFiltered(
+        bookmarkList.filter((product) => product.type === categoryType)
+      );
+    }
+  }, [categoryType, bookmarkList]);
+
+  const setCategory = (type) => {
+    setCategoryType(type);
+  };
   return (
     <div className="wrapper">
-      <div className="contentWrapper">
-        <div className="main">bookmark-page</div>
+      <div className="category-container">
+        <ProductCategory setCategory={setCategory} />
+      </div>
+      <div className="bookmark-list-product">
+        <Product
+          className="productPage-product"
+          productData={categoryType === "All" ? bookmarkList : filtered}
+        />
       </div>
     </div>
   );
